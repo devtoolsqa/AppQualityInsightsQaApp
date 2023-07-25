766843def4145a58dcc9484f82d3395e706f69c0
sandeeppadala <spadala88@gmail.com>
2023-07-25 10:06:06 -0700

    Added bew crash for vcs setting

--- app/src/main/java/com/example/appqualityinsightsqaapp/MainActivity.java
+++ app/src/main/java/com/example/appqualityinsightsqaapp/MainActivity.java
@@ -60,12 +60,8 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
+        throw new RuntimeException("Test Crash new");
     }
 
     @Override
