946a7fdcee4eb2ddc77265f97db0b0a0791635cf
ljimenezvargas <ljimenezvargas@google.com>
2023-07-25 14:30:29 -0700

    new RuntimeException in SecondFragment

--- app/src/main/java/com/example/appqualityinsightsqaapp/FirstFragment.java
+++ app/src/main/java/com/example/appqualityinsightsqaapp/FirstFragment.java
@@ -35,8 +35,8 @@ public class FirstFragment extends Fragment {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
 
-                String s=null;
-                int i=s.length();
+//                String s=null;
+//                int i=s.length();
             }
         });
     }
