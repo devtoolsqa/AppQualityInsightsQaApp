1b960d3dfa65e76ebc12549e1eb2f687aeb3f5a1
sandeeppadala <spadala88@gmail.com>
2023-07-27 14:22:03 -0700

    added flavors module

---
+++ flavorsModule/src/flv1/java/com/example/flavorsmodule/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.flavorsmodule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void createCrash(View view) throws Exception {
+        throw new Exception("flv1 crash");
+    }
+
+}
