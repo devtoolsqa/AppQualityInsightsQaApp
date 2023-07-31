68c7bbaac573501f201453cf23fadb95d3c0c898
sandeeppadala <spadala88@gmail.com>
2023-07-31 13:28:47 -0700

    Added methods

--- lib/src/main/java/com/example/lib/TestClass.java
+++ lib/src/main/java/com/example/lib/TestClass.java
@@ -3,6 +3,6 @@ package com.example.lib;
 public class TestClass {
     public static void Test()
     {
-
+TestClass2.Test();
     }
 }
--- lib/src/main/java/com/example/lib/TestClass2.java
+++ lib/src/main/java/com/example/lib/TestClass2.java
@@ -3,6 +3,6 @@ package com.example.lib;
 public class TestClass2 {
     public static void Test()
     {
-      TestClass2.Test();
+
     }
 }
