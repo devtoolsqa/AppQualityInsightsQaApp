4e658f9d2ea22947b1d6407358f5b103f0ec18e6
suyoglokhande <suyoglokhande@google.com>
2023-10-27 15:24:52 -0700

    Updating crash

--- mylibrary/src/main/java/com/example/mylibrary/AndroidLibraryClass.java
+++ mylibrary/src/main/java/com/example/mylibrary/AndroidLibraryClass.java
@@ -3,6 +3,6 @@ package com.example.mylibrary;
 public class AndroidLibraryClass {
 
     public static void createCrashInAndroidLibrary()  {
-        throw new NullPointerException("Android Library crash");
+        throw new RuntimeException("Android Library crash");
     }
 }
