68710ce1420ac86d917d962d395e09677407b899
sandeeppadala <spadala88@gmail.com>
2023-11-07 12:12:38 -0800

    Added Java library and Android library crashes in Second fragment

--- app/src/main/java/com/example/appqualityinsightsqaapp/FirstFragment.java
+++ app/src/main/java/com/example/appqualityinsightsqaapp/FirstFragment.java
@@ -35,8 +35,7 @@ public class FirstFragment extends Fragment {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
 
-                String s=null;
-                int i=s.length();
+
             }
         });
     }
--- app/src/main/java/com/example/appqualityinsightsqaapp/SecondFragment.java
+++ app/src/main/java/com/example/appqualityinsightsqaapp/SecondFragment.java
@@ -1,9 +1,13 @@
 package com.example.appqualityinsightsqaapp;
 
+import static com.example.lib.JavaLibraryClass.createCrashInJavaLibrary;
+import static com.example.mylibrary.AndroidLibraryClass.createCrashInAndroidLibrary;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,6 +18,7 @@ import com.example.appqualityinsightsqaapp.databinding.FragmentSecondBinding;
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
+
 
     @Override
     public View onCreateView(
@@ -34,6 +39,18 @@ public class SecondFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+        binding.andLibCrash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createCrashInAndroidLibrary();
+            }
+        });
+        binding.javaLibCrash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createCrashInJavaLibrary();
             }
         });
     }
