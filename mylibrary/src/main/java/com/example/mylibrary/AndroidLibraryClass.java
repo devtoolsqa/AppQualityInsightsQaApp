21701496a8a345fec26140417a1571e31c885410
suyog-google <109826951+suyog-google@users.noreply.github.com>
2024-02-13 10:43:13 -0800

    Changes for Multi-Event, Logs, Custom Keys, ANRs support

--- mylibrary/src/main/java/com/example/mylibrary/AndroidLibraryClass.java
+++ mylibrary/src/main/java/com/example/mylibrary/AndroidLibraryClass.java
@@ -1,8 +1,29 @@
 package com.example.mylibrary;
 
-public class AndroidLibraryClass {
+import com.google.firebase.crashlytics.CustomKeysAndValues;
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
+import android.content.Context;
 
+import java.util.Locale;
+
+
+public class AndroidLibraryClass {
+    public static Context context;
     public static void createCrashInAndroidLibrary()  {
+        String deviceLocale2 = Locale.getDefault().getDisplayLanguage();
+
+        CustomKeysAndValues keysAndValues = new CustomKeysAndValues.Builder()
+                .putString("library", "android_library")
+                .putString("device_locale", deviceLocale2)
+                .build();
+        FirebaseCrashlytics.getInstance().setCustomKeys(keysAndValues);
+        FirebaseCrashlytics.getInstance().log("Android Library crash");
+        //FirebaseCrashlytics crashlytics = FirebaseCrashlytics.getInstance();
+        //crashlytics.log("Android Library crash");
+
+        //context = context.getApplicationContext();
+        //crashlytics.setCustomKey("device_locale", deviceLocale2);
+
 
         throw new ExceptionInInitializerError ("Android Library crash");
     }
